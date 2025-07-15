2ebfb1548dacae7f5787179966f1d260ea34cfd3
Guigzz1404 <74819957+Guigzz1404@users.noreply.github.com>
2025-07-15 12:46:46 +0200

    Add files via upload

---
+++ 1_1_Data_Treatment.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import datetime
+import Process_LevelIII_Data
+
+
+# Display settings
+pd.options.display.max_columns = None
+pd.options.display.width = None
+
+
+# DATA TREATMENT
+# Read the LevelIII data
+csco_data = pd.read_csv("csco_levelIII_data.csv")
+dt = datetime.date(2025,7, 9)
+# Convert Timestamp column into datetime object
+csco_data["Timestamp"] = pd.to_datetime(csco_data["Timestamp"], unit="ms", origin=dt)
+csco_data = csco_data.drop(columns=["Ticker", "MPID"])
+
+# Process the LevelIII Data
+order_book, trades = Process_LevelIII_Data.process_book_data(csco_data)
+print(order_book) # Empty at the end of the day
+
